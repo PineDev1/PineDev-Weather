c7b891e1be21f5f16b9c69caf1d407c9ba8f63b0
Pine <83844501+Pineape123@users.noreply.github.com>
2026-09-05 12:54:10 -0600

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Riley Starkey on 2026-09-05.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
